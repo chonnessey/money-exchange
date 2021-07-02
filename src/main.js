4a3d619d0638a1ce5fea1ed88c70c929e46e62cc
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-02 11:44:09 -0700

    WIP: working on conversion logic

--- src/main.js
+++ src/main.js
@@ -2,5 +2,29 @@ import $ from 'jquery'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/styles.css'
+import CurrencyService from './currency-service'
 
-$("section").text("TEST TEXT INPUT")
+
+function getMoney(response) {
+  let country = $('#country').val();
+  let euroConversion = currency * response.conversion_rates.EUR
+  if(response) {
+    if(country === 'euro') {
+      $('#results').html(`The conversion in Euros is ${euroConversion}`)
+    }
+  }
+}
+
+let currency = $('#dollar').val()
+
+
+
+$(document).ready(function() {
+  $('#money').submit(function(event) {
+    event.preventDefault()
+    CurrencyService.getCurrency()
+      .then(function(response) {
+        getMoney(response)
+      })
+  })
+})
